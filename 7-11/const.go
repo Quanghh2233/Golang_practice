b009366216474533e2f69ce71098951b7dc36ab7
Quanghh2233 <93072281+Quanghh2233@users.noreply.github.com>
2024-11-07 15:41:58 +0700

    Update const.go

--- 7-11/const.go
+++ 7-11/const.go
@@ -1,17 +1,17 @@
-// package main
+  package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// )
+  import (
+  	"fmt"
+  	"math"
+  )
 
-// const s string = "constant"
+  const s string = "constant"
 
-// func main() {
-// 	fmt.Println(s)
-// 	const n = 5000000
-// 	const d = 3e20 / n
-// 	fmt.Println(d)
-// 	fmt.Println(int64(d))
-// 	fmt.Println(math.Sin(n))
-// }
+  func main() {
+  	fmt.Println(s)
+  	const n = 5000000
+  	const d = 3e20 / n
+  	fmt.Println(d)
+  	fmt.Println(int64(d))
+  	fmt.Println(math.Sin(n))
+  }
